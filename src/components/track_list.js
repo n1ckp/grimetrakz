f6c7a292f47eefdce668b22f2552c053ccd1206e
Nick Phillips <nicktgphillips@gmail.com>
2016-11-15 19:54:03 +0000

    now fetching top Tempa T tracks from Spotify

--- src/components/track_list.js
+++ src/components/track_list.js
@@ -1,23 +1,38 @@
 import React, { Component } from 'react';
 import TrackListItem from './track_list_item';
+import axios from 'axios';
+import querystring from 'querystring';
 
 export default class TrackList extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      tracks: [
-        {name: "Next Hype"},
-        {name: "Boi Off Da Ting"},
-        {name: "Shut Ya Mouth"},
-        {name: "Peng Ting"},
-        {name: "Mandem"}
-      ]
+      tracks: []
     };
   }
 
+  fetchTopTracks() {
+    // Spotify id for Tempa T (predetermined)
+    const id = "5itdSz26wZC57bo3dhQTPq";
+    const query = {
+      country: 'GB'
+    };
+    axios.get(`https://api.spotify.com/v1/artists/${id}/top-tracks?` + querystring.stringify(query))
+      .then((res) => {
+        this.setState({tracks: res.data.tracks.splice(0,5)});
+      });
+  }
+
+  componentDidMount() {
+    this.fetchTopTracks();
+  }
+
   renderTracks() {
+    if (this.state.tracks.length === 0) {
+      return (<p>Fetching tracks...</p>);
+    }
     return this.state.tracks.map((track) => {
-      return (<TrackListItem key={track.name} {...track} />);
+      return (<TrackListItem key={track.id} {...track} />);
     });
   }
 
