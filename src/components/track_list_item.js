cc4c33d78fec1d7706c0e8673af908d94668bd6f
Nick Phillips <nicktgphillips@gmail.com>
2016-11-16 10:14:36 +0000

    clean up css, added ellipsis trucation

--- src/components/track_list_item.js
+++ src/components/track_list_item.js
@@ -4,12 +4,13 @@ export default class TrackListItem extends Component {
   render() {
     return (
       <div className="row track">
-        <div className="small-6 columns">
-          <div className="track-player"><iframe src={`https://embed.spotify.com/?uri=spotify:track:${this.props.id}`} width="85" height="85" frameBorder="0" allowTransparency="true"></iframe></div>
-          <div className="track-name">{this.props.name}</div>
+        <div className="track-player">
+          <iframe src={`https://embed.spotify.com/?uri=spotify:track:${this.props.id}`} width="80" height="85" frameBorder="0" allowTransparency="true"></iframe>
         </div>
-        <div className="small-6 columns track-album-name">
-          {this.props.album.name} - <span className="track-number">Track {this.props.track_number}</span>
+        <div className="track-info">
+          <div className="track-name">{this.props.name}</div>
+          <div className="track-album-name">{this.props.album.name}</div>
+          <div className="track-number">Track {this.props.track_number}</div>
         </div>
       </div>
     );
