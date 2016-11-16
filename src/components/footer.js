373f9ccded01ce3ee8e732f14a87be80032253f5
Nick Phillips <nicktgphillips@gmail.com>
2016-11-16 10:21:43 +0000

    clean up footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,13 +1,12 @@
 import React, { Component } from 'react';
+import dateFormat from 'dateformat';
 
 export default class Footer extends Component {
   render() {
     return (
       <footer>
-        <p>
-          { '\u00A9' } Nick Phillips<span> - { Date().toString() }</span>
-        </p>
-
+        <span className="footer-name">{ '\u00A9' } Nick Phillips</span>
+        <span className="footer-date">{ dateFormat(Date(), "dddd dS mmmm yyyy") }</span>
       </footer>
     );
   }
